a7e4e230eb1731d3fb1b8208d04aaf039e11e548
RedkoVO <voredko@gmail.com>
2016-10-13 12:44:18 +0300

    Implementation removing users

--- app/controllers/pagination.js
+++ app/controllers/pagination.js
@@ -1,23 +1,45 @@
 'use strict'
 
 app.controller('Pagination', ['$scope', 'pagination', function ($scope, pagination) {
-    pagination.setProducts(users);
+    /* show pagination */
+    pagination.setUsers(users);
 
-    $scope.users = pagination.getPageProducts();
+    $scope.users = pagination.getPageUsers();
     $scope.paginationList = pagination.getPaginationList();
 
-    /*show pagination*/
     $scope.showPage = function(page) {
     		if (page == 'prev') {
     			   $scope.users = pagination.getPrevPageProducts();
     		} else if (page == 'next') {
     			   $scope.users = pagination.getNextPageProducts();
     		} else {
-    			   $scope.users = pagination.getPageProducts( page );
+    			   $scope.users = pagination.getPageUsers( page );
     		}
     }
 
     $scope.currentPageNum = function() {
   		  return pagination.getCurrentPageNum();
   	}
+
+    /* User delete */
+    $scope.remove = function(user) {
+        var index = $scope.users.indexOf(user);
+        $scope.users.splice(index, 1);
+    }
+
+    /* several user delete */
+    $scope.tableSelection = {};
+    $scope.removeSelectedRows = function() {
+         for (var i = $scope.users.length - 1; i >= 0; i--) {
+             if ($scope.tableSelection[i]) {
+                 $scope.users.splice(i, 1);
+                 delete $scope.tableSelection[i];
+             }
+         }
+   };
+
+
+    // $scope.filterAge = function(val) {
+    //     return (val.age > 10 && val.age < 30);
+    // };
 }]);
--- app/services/pagination.js
+++ app/services/pagination.js
@@ -1,68 +1,68 @@
 'use strict'
 
 app.service('pagination', function($sce) {
-	var currentPage = 0;      // начальная страница
-	var itemsPerPages = 10;   // сколько должно отображаться продуктов на странице
-	var products = [];        // будут храниться все объекты
+		var currentPage = 0;
+		var itemsPerPages = 10;
+		var users = [];
 
-	return {
-		setProducts: function( newProducts ) {
-			products = newProducts;
-		},
+		return {
+				setUsers: function( newUsers ) {
+						users = newUsers;
+				},
 
-		getPageProducts: function( num ){  //будет возвращать товары, которые на выбранной странице
-			var num = angular.isUndefined(num) ? 0 : num;
-			var first = itemsPerPages * num; //номер первого элемента
-			var last = first + itemsPerPages;
-			currentPage = num;
-			last = last > products.length ? (products.length - 1) : last;
-			return products.slice(first, last);
-		},
+				getPageUsers: function( num ){
+						var num = angular.isUndefined(num) ? 0 : num;
+						var first = itemsPerPages * num;
+						var last = first + itemsPerPages;
+						currentPage = num;
+						last = last > users.length ? (users.length - 1) : last;
+						return users.slice(first, last);
+				},
 
-		getTotalPagesNum: function() {
-			return Math.ceil( products.length / itemsPerPages );
-		},
+				getTotalPagesNum: function() {
+						return Math.ceil( users.length / itemsPerPages );
+				},
 
-		getPaginationList: function() {
-			var pagesNum = this.getTotalPagesNum();
-			var paginationList = [];
-			paginationList.push({
-				name: $sce.trustAsHtml('&laquo;'),
-				link: 'prev'
-			});
-			for (var i=0; i<pagesNum; i++) {
-				var name = i + 1;
-				paginationList.push({
-					name: $sce.trustAsHtml(String(name)),
-					link: i
-				});
-			};
-			paginationList.push({
-				name: $sce.trustAsHtml('&raquo'),
-				link: 'next'
-			});
-			if (pagesNum > 1) {
-				return paginationList;
-			} else {
-				return false;
-			}
-		},
+				getPaginationList: function() {
+						var pagesNum = this.getTotalPagesNum();
+						var paginationList = [];
+						paginationList.push({
+								name: $sce.trustAsHtml('&laquo;'),
+								link: 'prev'
+						});
+						for (var i=0; i<pagesNum; i++) {
+								var name = i + 1;
+								paginationList.push({
+										name: $sce.trustAsHtml(String(name)),
+										link: i
+								});
+						};
+						paginationList.push({
+								name: $sce.trustAsHtml('&raquo'),
+								link: 'next'
+						});
+						if (pagesNum > 1) {
+								return paginationList;
+						} else {
+								return false;
+						}
+				},
 
-		getCurrentPageNum: function() {
-			return currentPage;
-		},
+				getCurrentPageNum: function() {
+						return currentPage;
+				},
 
-		getPrevPageProducts: function() {
-			var prevPageNum = currentPage - 1;
-			if (prevPageNum < 0) prevPageNum = 0;
-			return this.getPageProducts(prevPageNum);
-		},
+				getPrevPageProducts: function() {
+						var prevPageNum = currentPage - 1;
+						if (prevPageNum < 0) prevPageNum = 0;
+						return this.getPageUsers(prevPageNum);
+				},
 
-		getNextPageProducts: function() {
-			var nextPageNum = currentPage + 1;
-			var pagesNum = this.getTotalPagesNum();
-			if (nextPageNum >= pagesNum) nextPageNum = pagesNum - 1;
-			return this.getPageProducts(nextPageNum);
+				getNextPageProducts: function() {
+						var nextPageNum = currentPage + 1;
+						var pagesNum = this.getTotalPagesNum();
+						if (nextPageNum >= pagesNum) nextPageNum = pagesNum - 1;
+						return this.getPageUsers(nextPageNum);
+				}
 		}
-	}
 });
